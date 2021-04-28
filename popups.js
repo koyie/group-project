64da663ab7b437cd4bfb92fd7e220fb29cc9a23a
KevinNelson08 <80488634+KevinNelson08@users.noreply.github.com>
2021-04-28 12:42:05 -0400

    Add files via upload

---
+++ popups.js
@@ -0,0 +1,137 @@
+//Popups for Encrypt
+//Opens Select File Popup for Encryption
+function opens1en() {
+    document.getElementById("modal1en").style.display="block"; 
+}
+
+//Closes Select File Popup for Encryption; Opens Password Popup for Encryption
+function closes1en() {
+    document.getElementById("modal1en").style.display="none"; 
+    document.getElementById("modal2en").style.display="block"; 
+}
+  
+//Closes Password Popup for Encryption; Opens Ready to download popup for Encryption
+function closes2en() {
+    document.getElementById("modal2en").style.display="none"; 
+    document.getElementById("modal3en").style.display="block"; 
+}
+
+//Closes Ready to download popup for Encryption
+function closes3en() {
+    document.getElementById("modal3en").style.display="none"; 
+}
+
+//User Cancel Select file popup for encryption
+function userClose1En() {
+    document.getElementById("modal1en").style.display="none"; 
+}
+
+//User Cancel Select file popup for encryption
+function userClose2En() {
+    document.getElementById("modal2en").style.display="none"; 
+}
+
+
+//Popups for Decrypt
+//Opens Select File Popup for decryption
+function opens1dec() {
+    document.getElementById("modal1dec").style.display="block"; 
+}
+
+//Closes Select File Popup for decryption; Opens Password Popup for decryption
+function closes1dec() {
+    document.getElementById("modal1dec").style.display="none"; 
+    document.getElementById("modal2dec").style.display="block"; 
+}
+
+//Closes password Popup for decryption; Opens Ready to download File Popup for decryption
+function closes2dec() {
+    document.getElementById("modal2dec").style.display="none"; 
+    document.getElementById("modal3dec").style.display="block"; 
+}
+
+//Closes Ready to download Popup for decyption
+function closes3dec() {
+    document.getElementById("modal3dec").style.display="none"; 
+}
+
+//User Cancel Select file popup for encryption
+function userClose1Dec() {
+    document.getElementById("modal1dec").style.display="none"; 
+}
+
+//User Cancel Select file popup for encryption
+function userClose2Dec() {
+    document.getElementById("modal2dec").style.display="none"; 
+}
+
+
+//Popups for Compress
+//Opens Select File Popup for compression
+function opens1com() {
+    document.getElementById("modal1com").style.display="block"; 
+}
+
+//Closes Select File Popup for compression; Opens password Popup for compresssion
+function closes1com() {
+    document.getElementById("modal1com").style.display="none"; 
+    document.getElementById("modal2com").style.display="block"; 
+}
+
+//Closes password Popup for compression; Opens ready to download Popup for compression
+function closes2com() {
+    document.getElementById("modal2com").style.display="none"; 
+    document.getElementById("modal3com").style.display="block"; 
+}
+
+//Closes ready to download Popup for compression
+function closes3com() {
+    document.getElementById("modal3com").style.display="none"; 
+}
+
+//User Cancel Select file popup for encryption
+function userClose1Com() {
+    document.getElementById("modal1com").style.display="none"; 
+}
+
+//User Cancel Select file popup for encryption
+function userClose2Com() {
+    document.getElementById("modal2com").style.display="none"; 
+}
+
+
+//Popus for Decompress
+//Opens Select File Popup for decompression
+function opens1decom() {
+    document.getElementById("modal1decom").style.display="block"; 
+}
+
+//Closes Select File Popup for decompression
+//Opens password Popup for decrompression
+function closes1decom() {
+    document.getElementById("modal1decom").style.display="none"; 
+    document.getElementById("modal2decom").style.display="block"; 
+}
+
+//Closes password Popup for decompression
+//Opens ready to download Popup for decompression
+function closes2decom() {
+    document.getElementById("modal2decom").style.display="none"; 
+    document.getElementById("modal3decom").style.display="block"; 
+
+}
+
+//Closes ready to download Popup for decompression
+function closes3decom() {
+    document.getElementById("modal3decom").style.display="none"; 
+}
+
+//User Cancel Select file popup for encryption
+function userClose1Decom() {
+    document.getElementById("modal1decom").style.display="none"; 
+}
+
+//User Cancel Select file popup for encryption
+function userClose2Decom() {
+    document.getElementById("modal2decom").style.display="none"; 
+}
