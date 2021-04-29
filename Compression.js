13528fc6eea93b0bee7319e366c36d19ed1aeee7
koyie <80488589+koyie@users.noreply.github.com>
2021-04-29 12:20:27 -0400

    Add files via upload

---
+++ Compression.js
@@ -0,0 +1,33 @@
+import java.io.*;
+import java.util.zip.*;
+
+public class Compression {
+	public static void compress (File source, File destination) throws IOException{
+		byte [] buffer = new byte [1024];
+		FileInputStream fis = new FileInputStream(source);
+        FileOutputStream fos = new FileOutputStream(destination);
+        GZIPOutputStream gzos = new GZIPOutputStream(fos);
+  
+        int read;
+        while ((read = fis.read(buffer)) != -1) {
+        	gzos.write(buffer, 0, read);
+        }
+        gzos.finish();
+        gzos.close();
+        fos.close();
+        fis.close();
+        
+	}
+	
+	
+	public static void main (String [] args) {
+		File source = new File ("");
+		File destination = new File ("");
+		try {
+			compress(source, destination);
+		}
+		catch(IOException e) {
+			System.out.println(e);
+		}
+	}
+}
